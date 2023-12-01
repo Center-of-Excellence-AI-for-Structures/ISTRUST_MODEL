2a312b524459e2b39ec708728e214914fff4809d
Panagiotis Komninos <panakom@hotmail.com>
2023-12-01 09:54:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     install_requires=[
         "pandas==1.5.3",
         "matplotlib==3.5.2",
-        "pillow==9.4.0",
+        "pillow==9.5.0",
         "scipy==1.10.1",
         "keyboard==0.13.5",
         "umap-learn==0.5.3",
