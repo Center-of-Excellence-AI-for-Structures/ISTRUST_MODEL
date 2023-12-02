ff0e5c66bd7fe1b7b546029583a0b52f7c9d875f
Panagiotis Komninos <panakom@hotmail.com>
2023-12-02 13:31:47 +0100

    Update utils.py

--- istrust_model/utils.py
+++ istrust_model/utils.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from scipy import stats
 import os
 import argparse
+import warnings
 
 
 def str2bool(v):
@@ -161,7 +162,7 @@ def search_folder(filename_dataset):
                 filename of the dataset (e.g. window_size_7)
         Returns:
             folder_name_test: str
-                the directory in which the relevant data is stored (e.g. runs/window_size_7_test_6-20_09_23-12_26)
+                the directory in which the relevant data is stored (e.g. runs/window_size_7_test_6-01_12_23-15_37)
     """
     name_folder = None
     for name_folder in os.listdir(f"runs"):
@@ -178,3 +179,106 @@ def search_folder(filename_dataset):
 
     folder_name_test = "runs/" + folder_name_test
     return folder_name_test
+
+
+def check_args(args):
+    """
+    Checks the boolean combinations of the parser arguments that are not allowed/do not make sense
+
+    Args:
+        args: arguments coming from parser
+
+    """
+
+    # Check if all (augmented) datasets have been created already
+    if not os.path.exists(f"dataset_processed"):
+        data_exists = False
+        data_aug_exists = False
+    else:
+        data_exists = True
+        data_aug_exists = True
+        for test_sample in args.test_samples:
+            name_dataset = f"window_size_7_test_{test_sample}_training_set.pth"
+            data_exists *= name_dataset in os.listdir(f"dataset_processed")
+            name_dataset = f"window_size_7_test_{test_sample}_training_set_augmented.pth"
+            data_aug_exists *= name_dataset in os.listdir(f"dataset_processed")
+
+    # Check if all models have been trained already
+    if not os.path.exists(f"runs"):
+        model_exists = False
+    else:
+        model_exists = True
+        for test_sample in args.test_samples:
+            name_dataset = f"window_size_7_test_{test_sample}"
+            in_folder = False
+            for candidate_name in os.listdir(f"runs"):
+                in_folder += name_dataset in candidate_name
+            model_exists *= in_folder
+
+    n_warnings = 0  # Track number of warnings to print to user
+    # All tests are below
+
+    if args.create_augmented_data and not args.create_data and not data_exists:
+        raise ValueError(f"create_augmented_data cannot be True if the dataset has not been created yet, "
+                         f"set create_data=True")
+
+    if not args.create_augmented_data and args.create_data:
+        warnings.warn(f"Creating data without creating augmented dataset "
+                      f"(create_augmented_data=False and create_data=True)")
+        n_warnings += 1
+
+    if args.data_augmentation and not args.create_augmented_data and not data_aug_exists:
+        raise ValueError(f"Cannot set data_augmentation=True if augmented dataset has not been created yet,"
+                         f"set create_augmented_data=True or data_augmentation=False")
+
+    if args.create_augmented_data and not args.data_augmentation:
+        warnings.warn(f"Creating augmented dataset without using it "
+                      f"(create_augmented_data=True and data_augmentation=False)")
+        n_warnings += 1
+
+    if args.train and not data_exists:
+        raise ValueError(f"Cannot train the model without creating the dataset,"
+                         f"set create_data=True")
+
+    if args.train and args.data_augmentation and not data_aug_exists:
+        raise ValueError(f"Cannot train the model with data_augmentation=True without creating the augmented dataset,"
+                         f"set create_augmented_data=True")
+
+    if args.train and not args.data_augmentation:
+        warnings.warn(f"Training the model without using augmented dataset, "
+                      f"training has not been set up nor tested for this")
+        n_warnings += 1
+
+    if args.test and not args.create_data and not data_exists:
+        raise ValueError(f"Cannot test the model without creating the dataset,"
+                         f"set create_data=True")
+
+    if args.test and not args.train and not model_exists:
+        raise ValueError(f"Cannot test the model without all trained models,"
+                         f"set train=True or download the model(s) from github")
+
+    if args.train_stage_2 and not args.create_data and not data_exists:
+        raise ValueError(f"Cannot perform second stage training without creating the dataset,"
+                         f"set create_data=True")
+
+    if args.train_stage_2 and not args.train:
+        warnings.warn(f"Training the model with both train=True and train_stage_2 is True, "
+                      f"the second stage training will be performed twice")
+        n_warnings += 1
+
+    if args.train_stage_2 and not args.train and not model_exists:
+        raise ValueError(f"Cannot perform second stage training without all (first stage) trained encoders,"
+                         f"set train=True or download the model(s) from github")
+
+    if args.export_attention_weights and not args.test:
+        raise ValueError(f"Cannot export attention weights if test=False, set test=True")
+
+    if args.export_umap and not args.create_augmented_data and not data_aug_exists:
+        raise ValueError(f"Cannot export UMAP representations if augmented data has not been created yet,"
+                         f"retrain the model with create_augmented_data=True and data_augmentation=True")
+
+    if args.export_umap and not args.test:
+        raise ValueError(f"Cannot export UMAP representations if test=False, set test=True")
+
+    # Print message to user that the test has passed including the amount of warnings that were triggered
+    print(f"Parser argument check passed with {n_warnings} warnings")
